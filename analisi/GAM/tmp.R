4026c9ebee40a52e70fbb36f2213b202085eb573
raffaele-morelli <raffaele.morelli@isprambiente.it>
2021-03-18 10:57:36 +0100

    Update del template con i grafici di dispersione, residui del gam.check Aggiunto codice di servizio su tmp.R

--- analisi/GAM/tmp.R
+++ analisi/GAM/tmp.R
@@ -1,5 +1,6 @@
 library(RPostgreSQL)
 library(dplyr)
+library(stringr)
 
 con <- dbConnect(PostgreSQL(), 
                  host = "10.158.102.164", 
@@ -19,17 +20,39 @@ df <- fetch(rs, n = -1)
 
 dbDisconnect(con)
 
+basedir <- "~/R/pulvirus"
+webdir <- "~/public_html/pulvirus"
 
 for(pltnt in names(df[-c(1,2, ncol(df))])) {
-  for( region_id in which(!is.na(df["c6h6"]))  ) {
+  for( region_id in which(!is.na(df[pltnt]))  ) {
     cat(glue::glue("Rscript scelta_var.R {pltnt} {region_id} & "), sep = "\n")
   }
 }
 
-basedir <- "~/R/pulvirus"
+rdatas <- list.files(path = glue("{basedir}/analisi/GAM/output"),
+                     pattern = "*.RData", 
+                     full.names = TRUE)
+
+for(i in rdatas) {
+  # f <- tools::file_path_sans_ext(i)
+  # print(f)
+  f <- tools::file_path_sans_ext(basename(i))
+  print(f)
+  regione <- stazioniAria %>% filter(region_id == str_split(f, "_")[[1]][2]) %>% select(regione) %>% unique()
+  
+  cat(
+    sprintf("rmarkdown::render(\"~/R/pulvirus/analisi/GAM/pulvirus_estrazioni_template.Rmd\",
+      params = list(rdatafile = \"%s\", titolo = \"%s\"),
+      output_dir = \"%s\",
+      output_file = \"%s\"
+              )", i, regione, webdir, f),
+    "\n"
+  )
+}
+
 # snip per generare i report ####
 rmarkdown::render("~/R/pulvirus/analisi/GAM/pulvirus_estrazioni_template.Rmd",
-                  params = list(rdatafile = glue("{basedir}/analisi/GAM/output/no2_2.RData"), titolo = "VDA"),
-                  output_dir = glue("{basedir}/analisi/GAM/report"),
-                  output_file = "no2_2"
+                  params = list(rdatafile = glue("{basedir}/analisi/GAM/output/no2_12.RData"), titolo = "Lazio"),
+                  output_dir = glue("{webdir}"),
+                  output_file = "no2_12"
                   )

d5f31ee1aee9403c5815e2e2735706e6f30b7f63
raffaele-morelli <raffaele.morelli@isprambiente.com>
2021-04-27 08:03:10 +0200

    Update Puglia e Shiny

--- analisi/GAM/GAMApp/ui.R
+++ analisi/GAM/GAMApp/ui.R
@@ -1,67 +1,114 @@
-
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(headerPanel(
-    title = "Navigazione dei dati e risultati",
-),
-
-fluidRow(
-    column(5,
-           conditionalPanel(condition="input.plot_tabs!='User guide'",
-                            tabsetPanel(id = "ui_tab",
-                                        tabPanel("Mappa",
-                                                 column(
-                                                     12,
-                                                     h4("Click su una stazione"),
-                                                     shinycssloaders::withSpinner(leaflet::leafletOutput("aqMap", height = "600px"),
-                                                                                  size = 2,
-                                                                                  color = "#0080b7"
-                                                     )
-                                                 )),
-                                        tabPanel("Tabella",
-                                                 column(
-                                                     12,
-                                                     h4("Cerca una stazione e seleziona la riga"), div(DT::dataTableOutput("table_input"), style = "font-size:90%")
-                                                 ))
-                            )
-           ),
-           conditionalPanel(condition="input.plot_tabs=='User guide'", column(12)
-           )
+shinyUI(fluidPage(
+  # Header
+  headerPanel(
+    title = tags$a(
+      href = 'https://pulvirus.isprambiente.it',
+      tags$img(
+        src = 'ispra_snpa.png',
+        height = 100,
+        width = 100 * 2.85 * 1.75
+      ),
+      target = "_blank"
     ),
-    column(7, tabsetPanel(
+    tags$head(
+      tags$link(rel = "icon", type = "image/png", href = "dwq_logo_small.png"),
+      windowTitle = "Pulvirus/GAM dashboard"
+    )
+  ),
+  
+  fluidRow(
+    column(
+      5,
+      conditionalPanel(condition = "input.plot_tabs!='Guida'",
+                       tabsetPanel(
+                         id = "ui_tab",
+                         tabPanel("Mappa", column(
+                           12,
+                           h4("Click su una stazione"),
+                           shinycssloaders::withSpinner(
+                             leaflet::leafletOutput("aqMap", height = "600px"),
+                             size = 2,
+                             color = "#0080b7"
+                           )
+                         )),
+                         tabPanel("Tabella", column(
+                           12,
+                           h4("Cerca una stazione e seleziona la riga"),
+                           div(DT::dataTableOutput("table_input"), style = "font-size:90%")
+                         ))
+                       )),
+      conditionalPanel(condition = "input.plot_tabs=='Guida'", column(12))
+    ),
+    column(
+      7,
+      tabsetPanel(
         id = "plot_tabs",
         tabPanel("Plot",
                  fluidRow(
-                     column(12,
-                            # uiOutput("date_slider"),
-                            selectInput("ts_pltnt", label = "Inquinante:", choices = c("NO2" = "no2", "PM10" = "pm10", "PM25" = "pm25")),
-                            radioButtons("ts_plot_type", "Tipo di plot:", choices = c("Serie", "Pollution rose", "Polar freq", "Boxplot"), inline = T),
-                            conditionalPanel(condition = "input.ts_plot_type=='Boxplot'", plotOutput("boxplot", width = "90%", height = "900px")),
-                            conditionalPanel(condition = "input.ts_plot_type=='Pollution rose'", plotOutput("pollution_rose")),
-                            conditionalPanel(condition = "input.ts_plot_type=='Serie'", plotOutput("serie", width = "90%", height = "500px")),
-                            conditionalPanel(condition = "input.ts_plot_type=='Polar freq'", plotOutput("polar"))
+                   column(
+                     12,
+                     style = "background-color: #f6f6f6; border-radius: .5em;",
+                     htmlOutput("nota_pltnt"),
+                     selectInput(
+                       "ts_pltnt",
+                       label = "Inquinante:",
+                       choices = c(
+                         "NO2" = "no2",
+                         "PM10" = "pm10",
+                         "PM25" = "pm25"
+                       )
                      )
+                   ),
+                   column(
+                     12,
+                     # uiOutput("date_slider"),
+                     radioButtons(
+                       "ts_plot_type",
+                       "Tipo di plot:",
+                       choices = c("Serie", "Pollution rose", "Polar freq", "Boxplot"),
+                       inline = T
+                     ),
+                     conditionalPanel(
+                       condition = "input.ts_plot_type=='Boxplot'",
+                       plotOutput("boxplot", width = "90%", height = "900px")
+                     ),
+                     conditionalPanel(condition = "input.ts_plot_type=='Pollution rose'", plotOutput("pollution_rose")),
+                     conditionalPanel(condition = "input.ts_plot_type=='Serie'", plotOutput(
+                       "serie", width = "90%", height = "500px"
+                     )),
+                     conditionalPanel(condition = "input.ts_plot_type=='Polar freq'", plotOutput("polar"))
+                   )
                  )),
         tabPanel("Descrittive",
-                 fluidRow(column(12,
-                                 div(DT::dataTableOutput("descrittive"), style = "font-size:90%")
+                 fluidRow(column(
+                   12, div(DT::dataTableOutput("descrittive"), style = "font-size:90%")
                  ))),
-        tabPanel("GAM",
-                 fluidRow(column(12,
-                                 # selectInput("ts_pltnt", label = "Inquinante:", choices = c("NO2" = "no2", "PM10" = "pm10", "PM25" = "pm25")),
-                                 htmlOutput("gam_output"),
-                                 htmlOutput("gam_summary"),
-                                 plotOutput("jd_plot"),
-                                 plotOutput("jd_plot_last"),
-                                 plotOutput("residui")
-                 ))
+        tabPanel(
+          "GAM",
+          fluidRow(column(
+            12,
+            # selectInput("ts_pltnt", label = "Inquinante:", choices = c("NO2" = "no2", "PM10" = "pm10", "PM25" = "pm25")),
+            htmlOutput("gam_output"),
+            htmlOutput("gam_summary"),
+            # plotOutput("jd_plot", width = "500px", height = "400px"),
+            # plotOutput("jd_plot_last", width = "500px", height = "400px"),
+            # plotOutput("residui", width = "600px", height = "600px")
+          )),
+          fluidRow(column(6, plotOutput("jd_plot")),
+                   column(6, plotOutput("jd_plot_last"))),
+          fluidRow(column(
+            12,
+            plotOutput("residui", width = "98%", height = "600px")
+          )),
         ),
         tabPanel("Guida",
-                 fluidRow(
-                     column(8,
-                            includeMarkdown('./guida/user_guide.rmd')
-                     )
-                 )
-        )
+                 fluidRow(column(
+                   8,
+                   includeMarkdown('./guida/user_guide.rmd')
+                 )))
         
-    ))
-),))
+      )
+    )
+  ),
+))
